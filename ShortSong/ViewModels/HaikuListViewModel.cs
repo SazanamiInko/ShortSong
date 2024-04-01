6ce2705b8a36bfc8979ba113e92957efd4a4d217
SazanamiInko <s.suzuki1614@gmail.com>
2024-04-01 16:55:03 +0900

    俳句検索画面の開発

---
+++ ShortSong/ViewModels/HaikuListViewModel.cs
@@ -0,0 +1,95 @@
+﻿using BLayer.DataModel;
+using FLayer;
+using MatBlazor;
+using Microsoft.AspNetCore.Components;
+
+namespace ShortSong.ViewModels
+{
+    /// <summary>
+    /// 俳句リストビューモデル
+    /// </summary>
+    public class HaikuListViewModel : ComponentBase
+    {
+        #region プロパティ
+
+        /// <summary>
+        /// キーワード
+        /// </summary>
+        [Parameter]
+        public string KeyWord { get; set; }
+
+        /// <summary>
+        /// お気に入り一覧
+        /// </summary>
+        public List<HaikuSimpleModel> Haikus { get; set; }
+
+        #endregion
+
+        #region サービス
+
+        /// <summary>
+        /// ダイアログ
+        /// </summary>
+        [Inject]
+        public IMatDialogService MatDialogService { get; set; }
+
+        /// <summary>
+        /// ナビ
+        /// </summary>
+        [Inject]
+        protected NavigationManager NavManager { get; set; }
+
+
+        #endregion
+
+        #region コンストラクタ
+
+        public HaikuListViewModel()
+        {
+            this.Haikus = new List<HaikuSimpleModel>();
+        }
+
+        #endregion
+
+        #region イベント
+        /// <summary>
+        /// 検索
+        /// </summary>
+        public async void OnSearch()
+        {
+
+            Search();
+
+            if (Haikus.Count == 0)
+            {
+                await MatDialogService.AlertAsync("俳句がありませんでした。");
+            }
+        }
+
+        public async void OnDelete(long id)
+        {
+
+        }
+
+        public async void OnEdit(long id)
+        {
+
+        }
+
+        #endregion
+
+        #region メソッド
+        /// <summary>
+        /// 検索処理
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        private void Search()
+        {
+            Haikus.Clear();
+            var records = FrontAPI.GetHaikus(this.KeyWord);
+            this.Haikus.AddRange((IEnumerable<HaikuSimpleModel>)records);
+        }
+        #endregion
+
+    }
+}
